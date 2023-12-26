7a5f0e655dc3b66317478a8751503f3d2b3e4903
Lucas Ribeiro <54214156+zeer0e1@users.noreply.github.com>
2023-12-26 11:06:58 -0300

    update
    Remove contact and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -4,11 +4,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html', context={'name': 'Lucas'})
-
-
-def contato(request):
-    return render(request, 'global/home.html')
-
-
-def sobre(request):
-    return HttpResponse('<h1>sobre</h1>')
